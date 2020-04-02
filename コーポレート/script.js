f4f01d1bf1f66c9be7434df39ed62529f264915a
R123 <47744466+R01234R@users.noreply.github.com>
2020-04-02 15:19:36 +0900

    Add files via upload

---
+++ コーポレート/script.js
@@ -0,0 +1,13 @@
+<script type="text/javascript">
+$(function() {
+　$('.Toggle').click(function() {
+　　$(this).toggleClass('active');
+
+　if ($(this).hasClass('active')) {
+　　$('.NavMenu').addClass('active');　 //クラスを付与
+　} else {
+　　$('.NavMenu').removeClass('active'); //クラスを外す
+　}
+　});
+});
+</script>
